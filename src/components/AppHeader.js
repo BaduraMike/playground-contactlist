b0ae272e5283d41b88f5a4072766f89f3d2e6022
Michal Badura <badura.mike@gmail.com>
2019-05-07 18:57:59 +0200

    Modify AppHeader, ContactItem, ContactList to class components.

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function AppHeader() {
-    return (
-        <header className="ui menu">
-            <nav className="ui container">
-                <a href="#" className="header item">
-                    <img
-                        className="logo"
-                        src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png"
-                        alt={"Logo"}
-                    />
-                    Contact List
-                </a>
-                <div className="header item">
-                    <button className="ui button">Add Contact</button>
-                </div>
-            </nav>
-        </header>
-    );
+class AppHeader extends Component {
+    render() {
+        return (
+            <header className="ui menu">
+                <nav className="ui container">
+                    <a href="#" className="header item">
+                        <img
+                            className="logo"
+                            src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png"
+                            alt={"Logo"}
+                        />
+                        Contact List
+                    </a>
+                    <div className="header item">
+                        <button className="ui button">Add Contact</button>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
 }
 
 export default AppHeader;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ContactItem from "./ContactItem";
 
-function ContactList() {
-    return (
-        <div className={"ui relaxed divided list selection"}>
-            <ContactItem
-                login={"typeofweb1"}
-                name={"Lena"}
-                department={"JavaScript Developer"}
-            />
-            <ContactItem
-                login={"typeofweb2"}
-                name={"Brian"}
-                department={"Human Resources"}
-            />
-            <ContactItem
-                login={"typeofweb3"}
-                name={"Rick"}
-                department={"QA"}
-            />
-            <ContactItem
-                login={"typeofweb4@pl"}
-                name={"Emyl"}
-                department={"E-Mailing Team"}
-            />
-        </div>
-    );
+class ContactList extends Component {
+    render() {
+        return (
+            <div className={"ui relaxed divided list selection"}>
+                <ContactItem
+                    login={"typeofweb1"}
+                    name={"Lena"}
+                    department={"JavaScript Developer"}
+                />
+                <ContactItem
+                    login={"typeofweb2"}
+                    name={"Brian"}
+                    department={"Human Resources"}
+                />
+                <ContactItem
+                    login={"typeofweb3"}
+                    name={"Rick"}
+                    department={"QA"}
+                />
+                <ContactItem
+                    login={"typeofweb4@pl"}
+                    name={"Emyl"}
+                    department={"E-Mailing Team"}
+                />
+                <ContactItem
+                    login={"emailing41@pl"}
+                    name={"Emylianho"}
+                    department={"E-Mailing Team"}
+                />
+            </div>
+        );
+    }
 }
 
 export default ContactList;
