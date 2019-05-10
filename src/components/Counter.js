6f5e2f3684729dca85423dd76ce238b96ee54048
Michal Badura <badura.mike@gmail.com>
2019-05-10 20:43:52 +0200

    Modify Counter - add callback to increment, decrement functions.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,71 +14,67 @@ class Counter extends Component {
 
         return (
             <div className={"middle aligned columns"}>
-                        <p><br/>
-                            <div className={"ui buttons"}>
-                                <button className={"ui animated button"}
-                                        onClick={this.clickIncrement.bind(this)}
-                                        onDoubleClick={this.doubleClickIncrement.bind(this)}>
-                                    <div className={"visible content"}>Like</div>
-                                    <div className={"hidden content"}>
-                                        <i className={"plus icon"}/>
-                                    </div>
-                                </button>
-                                <div className={"or"}/>
-                                <button className={"ui animated button"}
-                                        onClick={this.clickDecrement.bind(this)}
-                                        onDoubleClick={this.doubleClickDecrement.bind(this)}>
-                                    <div className={"visible content"}>Dislike</div>
-                                    <div className={"hidden content"}>
-                                        <i className={"minus icon"}/>
-                                    </div>
-                                </button>
+                <p><br/>
+                    <div className={"ui buttons"}>
+                        <button className={"ui animated button"}
+                                onClick={this.clickIncrement.bind(this)}
+                                onDoubleClick={this.doubleClickIncrement.bind(this)}>
+                            <div className={"visible content"}>Like</div>
+                            <div className={"hidden content"}>
+                                <i className={"plus icon"}/>
                             </div>
-                        </p>
-                        <div className={"ui label"}>
-                            <p>Current social status: {this.state.sumCounter}</p>
-                        </div>
-                        <div className={"ui label"}>
-                            <p>All reactions: {this.state.totalCounter}</p>
-                        </div>
-                        <div className={"ui label"}>
-                            <p>Nervous reactions: {this.state.doubleClickCounter}</p>
-                        </div>
+                        </button>
+                        <div className={"or"}/>
+                        <button className={"ui animated button"}
+                                onClick={this.clickDecrement.bind(this)}
+                                onDoubleClick={this.doubleClickDecrement.bind(this)}>
+                            <div className={"visible content"}>Dislike</div>
+                            <div className={"hidden content"}>
+                                <i className={"minus icon"}/>
+                            </div>
+                        </button>
+                    </div>
+                </p>
+                <div className={"ui label"}>
+                    <p>Current social status: {this.state.sumCounter}</p>
+                </div>
+                <div className={"ui label"}>
+                    <p>All reactions: {this.state.totalCounter}</p>
+                </div>
+                <div className={"ui label"}>
+                    <p>Nervous reactions: {this.state.doubleClickCounter}</p>
+                </div>
             </div>
         );
     }
 
     clickIncrement() {
-        this.setState(prevState => {
-            return {
-                sumCounter: prevState.sumCounter + 1,
-                totalCounter: prevState.totalCounter + 1,
-            };
+        this.setState({
+            sumCounter: this.state.sumCounter + 1,
+            totalCounter: this.state.totalCounter + 1,
+        }, () => {
         });
     }
 
     clickDecrement() {
-        this.setState(prevState => {
-            return {
-                sumCounter: prevState.sumCounter - 1,
-                totalCounter: prevState.totalCounter + 1,
-            };
+        this.setState({
+            sumCounter: this.state.sumCounter - 1,
+            totalCounter: this.state.totalCounter + 1,
+        }, () => {
         });
     }
 
     doubleClickIncrement() {
-        this.setState(prevState => {
-            return {
-                doubleClickCounter: prevState.doubleClickCounter + 1,
-            };
+        this.setState({
+            doubleClickCounter: this.state.doubleClickCounter + 1,
+        }, () => {
         });
     }
 
     doubleClickDecrement() {
-        this.setState(prevState => {
-            return {
-                doubleClickCounter: prevState.doubleClickCounter + 1,
-            };
+        this.setState({
+            doubleClickCounter: this.state.doubleClickCounter + 1,
+        }, () => {
         });
     }
 }
